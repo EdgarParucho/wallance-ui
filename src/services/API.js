4e937d934ec9d75a662f349ae4b293fd8c243915
Edgar Parucho <parucho.edgar@outlook.es>
2023-07-28 17:39:04 -0400

    API URL updated

--- src/services/API.js
+++ src/services/API.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:3000/api' });
+const API = axios.create({ baseURL: 'https://wallance.onrender.com/api' });
 
 export default API;
