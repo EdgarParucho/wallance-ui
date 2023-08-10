bba5005b6a5afeaeb285153c798be7bbc712b451
Edgar Parucho <parucho.edgar@outlook.es>
2023-08-10 18:17:06 -0400

    Adding NavBar.vue

---
+++ src/components/layout/NavBar.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { ref, computed, defineAsyncComponent } from 'vue';
+import { useRoute } from 'vue-router';
+import { PlusIcon, ArrowsRightLeftIcon, CircleStackIcon, ArchiveBoxIcon } from '@heroicons/vue/24/outline';
+import { storeToRefs } from 'pinia';
+import { useFundStore } from '../../stores/fundStore';
+
+const route = useRoute();
+const fundStore = useFundStore();
+const { funds } = storeToRefs(fundStore);
+const atHome = computed(() => route.fullPath === '/');
+
+const RecordForm = defineAsyncComponent(() => import('../record/RecordForm.vue'));
+const AssignmentForm = defineAsyncComponent(() => import('../record/AssignmentForm.vue'));
+
+const links = [
+  { hint: "Funds", path: "/funds", icon: ArchiveBoxIcon },
+  { hint: "Records", path: "/records", icon: CircleStackIcon },
+];
+let recordFormIsOpen = ref(false);
+let assignmentFormIsOpen = ref(false);
+</script>
+
+<template>
+  <div v-if="!atHome" class="flex justify-around items-center px-1 w-full text-white bg-stone-900 sticky bottom-0">
+    <button
+    class="grid justify-center pt-1 h-full w-full font-semibold text-white bg-stone-800 hover:bg-stone-700"
+    @click="recordFormIsOpen = true"
+    >
+      <PlusIcon class="text-yellow-400 w-6 mx-auto" />
+      <small>Record</small>
+    </button>
+
+    <router-link
+    v-for="link, i in links" :key="i"
+    :to="link.path"
+    class="grid justify-center pt-1 h-full w-full bg-stone-800 text-white hover:bg-stone-700"
+    :class="{ 'bg-stone-600': (route.fullPath === link.path) }"
+    >
+      <component :is="link.icon" class="w-6 mx-auto" />
+      <small>{{ link.hint }}</small>
+    </router-link>
+
+    <button
+    @click="assignmentFormIsOpen = true"
+    :disabled="funds.length < 2"
+    class="grid justify-center pt-1 h-full w-full border-white font-semibold text-white bg-stone-800 hover:bg-stone-700"
+    >
+      <ArrowsRightLeftIcon class="text-yellow-400 w-6 mx-auto" />
+      <small>Assign</small>
+    </button>
+    <RecordForm v-if="recordFormIsOpen" :form-is-open="recordFormIsOpen" @close-form="recordFormIsOpen = false" :editing="false" />
+    <AssignmentForm v-if="assignmentFormIsOpen" :form-is-open="assignmentFormIsOpen" @close-form="assignmentFormIsOpen = false" />
+  </div>
+</template>
