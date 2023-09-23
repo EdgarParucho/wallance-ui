31cd0d357c4080d5829b85b32e989cd78ef46be5
EdgarParucho <parucho.edgar@outlook.es>
2023-09-23 19:17:49 -0400

    removing comment

--- src/services/API.js
+++ src/services/API.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-// const API = axios.create({ baseURL: 'https://wallance.onrender.com/api' });
 const API = axios.create({
   baseURL: import.meta.env.PROD ? 'https://wallance.onrender.com/api' : 'http://localhost:3000/api'
 });
